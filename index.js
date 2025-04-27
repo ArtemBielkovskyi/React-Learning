d0dc51691620267369ec750f2bcf06b71e73fbba
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-04-27 10:46:15 +0100

    Created react environment

--- index.js
+++
@@ -1,2 +0,0 @@
-// ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
-// ReactDOM.render(<h1>What a great start</h1>, document.getElementById('root2'));
---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default App;
