98258fe0899d85e75816a7bdc7725dd86692145f
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-04-27 11:09:35 +0100

    Started practising

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const username = "Artem";
+
+function Button() {
   return (
-    <button>I'm a button</button>
+    <button className="button1">I'm a button</button>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <h1>Hello world! React time!</h1>
+      <Button />
+      <h2>This website under the license of <span className="creator">{username}</span></h2>
+    </>
+  );
+}
