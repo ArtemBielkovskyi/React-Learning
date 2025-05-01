929c4b71053b848f651ff4470e674e70748ef475
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-05-01 09:04:44 +0100

    Covered basics of React from official docs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-const username = "Artem";
+const username = "Artem1";
 
 function Button() {
   return (
-    <button className="button1">I'm a button</button>
+    <button className="button1">Sign in</button>
   );
+}
+
+function AlertMessage() {
+  const [count, setCount] = useState(0);
+  function Alert(){
+    alert("Hello, "+username+"!");
+    setCount(count + 1)
+  }
+  return (
+    <button className="button2" onClick={Alert}>Clicked {count} times</button>
+  )
 }
 
 export default function App() {
@@ -14,11 +26,12 @@ export default function App() {
     if(username === "Artem") {
       content = <MainMenu />;
     } else {
-      content = <h1>Welcome, {username}!</h1>;
+      content = <><h1>Welcome, {username}!</h1><Button /><AlertMessage /></>;
     }
     return(
       <div>
         {content}
+        <List />
       </div>
     )
 }
@@ -36,4 +49,23 @@ function MainMenu(){
       </div>
       </>
     );
+}
+
+function List() {
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+  const listItems = products.map(product =>
+    <li key={product.id} style={{
+      color: product.id == 1 ? 'magenta' : 'darkgreen'
+    }}>
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
 }
